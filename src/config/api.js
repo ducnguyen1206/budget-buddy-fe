c011fd8c81bcc288b319f767d4ec800159e7c2a7
ducnguyen1206 <minhduc12037@gmail.com>
2025-12-25 22:57:41 +0800

    Update API

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,8 @@ import { getAuthHeader } from "../utils/tokenManager";
 
 // API Configuration
 // Use environment variable or fallback to localhost for development
-export const API_BASE_URL =  "budget-buddy-be-11xo2-web.budget-buddy-be-11xo2.svc.cluster.local";
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
 //
 // export const API_BASE_URL = "https://18.142.242.248.nip.io";
 
